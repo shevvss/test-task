6d524470facb204e6ab1ef2f28f24c563c36eb21
shevvss <marin.shevchik@gmail.com>
2022-07-29 04:33:46 +0400

    Updates

--- index.bundle.js
+++ index.bundle.js
@@ -223,7 +223,7 @@ eval("__webpack_require__(/*! ./mystyles.scss */ \"./src/mystyles.scss\");\n\n\n
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f0554d65296aae1a121b")
+/******/ 		__webpack_require__.h = () => ("97b4d99461f5f2b8a902")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
